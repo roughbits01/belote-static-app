351a4c4bc051af81900b519b33b212ddc18ce73e
Marwen Azouzi <marwen.azouzi@datadoghq.com>
2025-05-17 22:24:47 +0200

    release

--- index-1747513487.service.worker.js
+++ index-1747513487.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747513206|579653772';
+const CACHE_VERSION = '1747513485|859034311';
 /** @type {string} */
 const CACHE_PREFIX = 'Belote-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
